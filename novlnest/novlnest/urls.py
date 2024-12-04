43d8281db10e96d295c0f1df2843727c9919403a
vedanthanekar45 <vedanthanekar45@gmail.com>
2024-12-04 15:00:47 +0530

    tried :(

--- novlnest/novlnest/urls.py
+++ novlnest/novlnest/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import *
 
 urlpatterns = [
+    path("", client),
     path('admin/', admin.site.urls),
 ]
